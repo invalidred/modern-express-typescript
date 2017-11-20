09a37f19bfe215008babc8a409f1b9cd65bcba90
Abdul Khan <invalidred@gmail.com>
2017-11-20 06:38:37 -0500

    intermediate tests

---
+++ src/__tests__/add_es5.test.js
@@ -0,0 +1,5 @@
+import add from '../add_es5';
+
+test('should add', () => {
+  expect(add(2, 5)).toEqual(7);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
-import * as express from 'express';
+import express, { Request, Response } from 'express';
+
 const app = express();
 
 app.use(express.static(`${__dirname}/../public`));
 
 app.get('/', (req: express.Request, res: express.Response) => {
-  // tslint:disable-next-line:no-console
-  console.log('hi there');
   res.send('Hello word');
 });
 
-app.listen(process.argv[2], () => {
-  // tslint:disable-next-line:no-console
-  console.log(`modern-express-typescript listening on ${process.argv[2]}`);
-  // tslint:disable-next-line:no-console
-  console.log('This is updated');
-});
+export function startServer() {
+  app.listen(process.argv[2], () => {
+    // tslint:disable-next-line:no-console
+    console.log(`modern-express-typescript listening on ${process.argv[2]}`);
+  });
+}
+
+startServer();
